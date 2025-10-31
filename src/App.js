ff75ba374a2b0b085b70f9ac9aeeb78a0faa9d2d
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-31 09:14:26 +0000

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>NFT Văn Hoá</h1>
+        <p>Khám phá và bảo tồn văn hoá Việt Nam qua NFT.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
