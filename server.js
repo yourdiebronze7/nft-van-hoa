a44f043e2d76872597f767d7a40839fa0fd2ab3a
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-31 09:15:19 +0000

    Update welcome message for clarity

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Chào mừng đến với NFT Văn Hoá!');
+    res.send('Welcome to NFT Cultural');
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server đang chạy trên cổng ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
